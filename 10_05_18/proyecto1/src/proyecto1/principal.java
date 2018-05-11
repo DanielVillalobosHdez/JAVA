2c0ee67b491cc4731c016b13dc3ffccef3d686d0
danielvillaloboshdez <danivillalobos1997@gmail.com>
2018-05-11 10:32:25 +0200

    JAVA

---
+++ 10_05_18/proyecto1/src/proyecto1/principal.java
@@ -0,0 +1,24 @@
+package proyecto1;
+
+public class principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// tipos de variebles más conocidas
+		
+		int numero = 7;
+		double decimal = 9.5;
+		char caracter = 'a';
+		String palabra = "achus";
+		boolean soyhumano = true;
+		
+		System.out.println("El decimal es:\n" + decimal);
+		System.out.println("\n");
+		System.out.println("Oscar ha hecho:\t\t" + palabra);
+
+		
+		
+	}
+
+}
